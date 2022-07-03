0580012e0f11601c334efefb497807918b862467
noah <nmatsuyoshi@gmail.com>
2022-07-03 01:54:30 -0700

    small fix

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.post('/login', async function(req, res) {
     const password = globalConstants.sanitize(req.body.password);
     console.log("http req made");
     let user = await dynamoHelper.getUser(req.body.username);
-    if(!user.password) {
+    if(!('password' in user) && !user.password) {
         await dynamoHelper.setPassword(username, password);
         user = await dynamoHelper.getUser(req.body.username);
     }
